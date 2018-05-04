7b3e85698e8e445e5f779d802482998975e309aa
jared <jaredspecjr@gmail.com>
2018-05-04 15:24:14 -0700

    attempt to create pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CategoryPipe } from './category.pipe';
 import { NewsListComponent } from './news-list/news-list.component';
 import { ShowInfoComponent } from './show-info/show-info.component';
 import { CommentsComponent } from './comments/comments.component';
@@ -14,7 +14,8 @@ import { CommentsComponent } from './comments/comments.component';
     AppComponent,
     NewsListComponent,
     ShowInfoComponent,
-    CommentsComponent
+    CommentsComponent,
+    CategoryPipe
 
   ],
   imports: [
---
+++ src/app/category.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { NewsStory } from './models/newsStory.model';
+
+@Pipe({
+  name:"category",
+  pure:false
+})
+
+export class CategoryPipe implements PipeTransform {
+  transform(input: NewsStory[]) {
+    let output: NewsStory[] =[];
+    for (let i = 0; i < input.length; i++) {
+      if (input[i].category === 1) {
+        output.push(input[i]);
+      }
+    }
+    return output;
+  }
+}
