f3122d346a3058d58115039e2e0369c05e293c32
jared <jaredspecjr@gmail.com>
2018-05-04 16:03:44 -0700

    commented out pipe for now until i can figure out the right way to implement it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { CategoryPipe } from './category.pipe';
+// import { CategoryPipe } from './category.pipe';
 import { NewsListComponent } from './news-list/news-list.component';
 import { ShowInfoComponent } from './show-info/show-info.component';
 import { CommentsComponent } from './comments/comments.component';
@@ -14,8 +14,8 @@ import { CommentsComponent } from './comments/comments.component';
     AppComponent,
     NewsListComponent,
     ShowInfoComponent,
-    CommentsComponent,
-    CategoryPipe
+    CommentsComponent
+    // CategoryPipe
 
   ],
   imports: [
--- src/app/category.pipe.ts
+++ src/app/category.pipe.ts
@@ -1,20 +1,22 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { NewsStory } from './models/newsStory.model';
-
-@Pipe({
-  name:"category",
-  pure:false
-})
-
-export class CategoryPipe implements PipeTransform {
-  transform(input: NewsStory[], desiredCategory) {
-    let output: NewsStory[] =[];
-    if(desiredCategory === "allStories")
-    for (let i = 0; i < input.length; i++) {
-      if (input[i].category === 1) {
-        output.push(input[i]);
-      }
-    }
-    return output;
-  }
-}
+// import { Pipe, PipeTransform } from '@angular/core';
+// import { NewsStory } from './models/newsStory.model';
+//
+// @Pipe({
+//   name:"category",
+//   pure:false
+// })
+//
+// export class CategoryPipe implements PipeTransform {
+//   transform(input: NewsStory[], desiredCategory) {
+//     let output: NewsStory[] =[];
+//     if(desiredCategory === "allStories")
+//     for (let i = 0; i < input.length; i++) {
+//       if (input[i].category === 1) {
+//         output.push(input[i]);
+//       } else if {
+//
+//       }
+//     }
+//     return output;
+//   }
+// }
