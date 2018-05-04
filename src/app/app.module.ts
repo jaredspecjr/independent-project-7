1def5d205c02a6aa3535e7c65d2bb6498141ab16
jared <jaredspecjr@gmail.com>
2018-05-04 12:57:09 -0700

    add new component for the comment section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NewsListComponent } from './news-list/news-list.component';
 import { ShowInfoComponent } from './show-info/show-info.component';
+import { CommentsComponent } from './comments/comments.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsListComponent,
-    ShowInfoComponent
+    ShowInfoComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
