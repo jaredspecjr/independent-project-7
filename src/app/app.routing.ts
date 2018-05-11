a861909aaa297392cc2cba63c7c48375eadc9068
jared <jaredspecjr@gmail.com>
2018-05-11 09:10:52 -0700

    successfully use new test route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,16 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewsListComponent } from './news-list/news-list.component';
-
+import { ShowInfoComponent } from './show-info/show-info.component';
 
 const appRoutes: Routes = [
   {
-    path:'',
+    path: '',
     component: NewsListComponent
+  },
+  {
+    path:'show-info',
+    component: ShowInfoComponent
   }
 ];
 
