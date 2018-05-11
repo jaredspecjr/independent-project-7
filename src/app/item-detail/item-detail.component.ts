dc701e8428ec866dfec00767514582e0835d930b
jared <jaredspecjr@gmail.com>
2018-05-11 10:35:54 -0700

    add ability to git different information depending on what title you click

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { NewsStory } from '../newsStory.model';
 
 @Component({
   selector: 'app-item-detail',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-detail.component.css']
 })
 export class ItemDetailComponent implements OnInit {
-
-  constructor() { }
+  newsStoryId: number = null;
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.newsStoryId = parseInt(urlParameters['id']);
+    });
   }
 
 }
